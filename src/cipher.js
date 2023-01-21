a2caf261669d1d1c4662fc8297050adb7edbc123
Ariane <arianevieira26@gmail.com>
2023-01-21 19:01:30 -0300

    cipher.js pronto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
+const alfabeto = 26;
+const codigoAscii = 65;
+
 const cipher = {
-  // ...
+  encode: function encode(offset, string) {
+    if (offset == null || string) {
+      throw new TypeError("Erro");
+    }
+
+    let resultadoCifra = "";
+    for (let i = 0; i < string.length; i++) {
+      const criptar = string.charCodeAt(i);
+      let codificar =
+        ((criptar - codigoAscii + offset) % alfabeto) + codigoAscii;
+      if (codificar <= 64 || codificar >= 91) {
+        criptar = codificar;
+      }
+      resultadoCifra += string.charCodeAt(codificar);
+    }
+    return resultadoCifra;
+  },
+
+  decode: function decode(offset, string) {
+    if (offset == null || string) {
+      throw new TypeError("Erro");
+    }
+
+    let resultadoDecifra = "";
+    for (let i = 0; i < string.length; i++) {
+      const decriptar = string.charCodeAt(i);
+      let decodificar =
+        ((decriptar + codigoAscii - offset) % alfabeto) + codigoAscii;
+      if (decodificar <= 64 || decodificar >= 91) {
+        decriptar = decodificar;
+      }
+      resultadoDecifra += string.fromCharCode(decodificar);
+    }
+    return resultadoDecifra;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import cipher from './cipher.js';
-
+import cipher from "./cipher.js";
 console.log(cipher);
+
+//Modificações títulos e subtítulo por ID
+
+let titulo = document.querySelector("#titulo");
+titulo.innerHTML = "Papo de busão";
+
+let subTitulo = document.querySelector("#subTitulo");
+subTitulo.innerHTML =
+  "Não corra o risco de ter suas mensagem lida por aqueles que estão ao seu redor no busão. Já viu o tanto de pessoas que tem suas mensagem fotografadas e acabam virando meme?<br> Se sua intenção não é ser famoso dessa forma, use esse App que você ficará seguro quando estiver em locais com muitas pessoas.";
+
+//Evento dos botões
+const btn = document.querySelector("#btnCodificar");
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const name = document.querySelector("#codificado");
+  const value = name.value;
+  //console.log(value);
+});
+
+const btn2 = document.querySelector("#btnDecodificar");
+btn2.addEventListener("click", function (e) {
+  e.preventDefault();
+  const name2 = document.querySelector("#decodificar");
+  const value2 = name2.value;
+  // console.log(value2);
+});

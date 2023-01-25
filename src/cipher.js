e6cda815364822274e47a05f184686a006b041c8
Ariane <arianevieira26@gmail.com>
2023-01-25 13:09:11 -0300

    cipher.js corrigido e dentro das especificacoes eslint

--- src/cipher.js
+++ src/cipher.js
@@ -3,37 +3,33 @@ const codigoAscii = 65;
 
 const cipher = {
   encode: function encode(offset, string) {
-    if (offset == null || string) {
-      throw new TypeError("Erro");
+    if (offset !== "number" || string !== "string") {
+      throw new TypeError("Escolha um número de deslocamento");
     }
 
     let resultadoCifra = "";
-    for (let i = 0; i < string.length; i++) {
-      const criptar = string.charCodeAt(i);
-      let codificar =
-        ((criptar - codigoAscii + offset) % alfabeto) + codigoAscii;
-      if (codificar <= 64 || codificar >= 91) {
-        criptar = codificar;
-      }
-      resultadoCifra += string.charCodeAt(codificar);
+    for (let i = 0; i < String.length; i++) {
+      const char = string[i];
+      const codificar =
+        ((char.charCodeAt(0) - codigoAscii + offset + alfabeto) % alfabeto) +
+        codigoAscii;
+      resultadoCifra += String.fromCharCode(codificar);
     }
     return resultadoCifra;
   },
 
   decode: function decode(offset, string) {
-    if (offset == null || string) {
-      throw new TypeError("Erro");
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError("Escolha um número de deslocamento");
     }
 
     let resultadoDecifra = "";
-    for (let i = 0; i < string.length; i++) {
-      const decriptar = string.charCodeAt(i);
-      let decodificar =
-        ((decriptar + codigoAscii - offset) % alfabeto) + codigoAscii;
-      if (decodificar <= 64 || decodificar >= 91) {
-        decriptar = decodificar;
-      }
-      resultadoDecifra += string.fromCharCode(decodificar);
+    for (let i = 0; i < String.length; i++) {
+      const char2 = string[i];
+      const decodificar =
+        ((char2.charCodeAt(0) + codigoAscii - offset + alfabeto) % alfabeto) +
+        codigoAscii;
+      resultadoDecifra += String.fromCharCode(decodificar);
     }
     return resultadoDecifra;
   },
--- src/index.js
+++ src/index.js
@@ -3,26 +3,31 @@ console.log(cipher);
 
 //Modificações títulos e subtítulo por ID
 
-let titulo = document.querySelector("#titulo");
+const titulo = document.querySelector("#titulo");
 titulo.innerHTML = "Papo de busão";
 
-let subTitulo = document.querySelector("#subTitulo");
+const subTitulo = document.querySelector("#subTitulo");
 subTitulo.innerHTML =
   "Não corra o risco de ter suas mensagem lida por aqueles que estão ao seu redor no busão. Já viu o tanto de pessoas que tem suas mensagem fotografadas e acabam virando meme?<br> Se sua intenção não é ser famoso dessa forma, use esse App que você ficará seguro quando estiver em locais com muitas pessoas.";
 
-//Evento dos botões
-const btn = document.querySelector("#btnCodificar");
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  const name = document.querySelector("#codificado");
-  const value = name.value;
-  //console.log(value);
-});
+//Evento botão Criptografar
 
-const btn2 = document.querySelector("#btnDecodificar");
-btn2.addEventListener("click", function (e) {
-  e.preventDefault();
-  const name2 = document.querySelector("#decodificar");
-  const value2 = name2.value;
-  // console.log(value2);
-});
+const codificar = document.querySelector("#codificar");
+function btn1() {
+  const texto1 = document.querySelector("#texto1").value.toUpperCase();
+  const offset = document.querySelector("#offset");
+  const textoCodificado = cipher.encode(offset, texto1);
+  document.querySelector("#texto1").value = textoCodificado;
+}
+codificar.addEventListener("click", btn1);
+
+// Evento botão descriptografar
+
+const decodificar = document.querySelector("#decodificar");
+function btn2() {
+  const texto2 = document.querySelector("#texto2").value.toUpperCase();
+  const offset = document.querySelector("#offset");
+  const textoDecodificado = cipher.decode(offset, texto2);
+  document.querySelector("#texto2").value = textoDecodificado;
+}
+decodificar.addEventListener("click", btn2);
